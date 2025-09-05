0ac168428b314b4fc9e5082fd0631131e35bbb53
rafa1478 <rafamohammed1997@gmail.com>
2025-09-05 16:49:57 +0300

    h.w6

---
+++ h.w6/my_homework/bin/my_homework.dart
@@ -0,0 +1,5 @@
+import 'package:my_homework/my_homework.dart' as my_homework;
+
+void main(List<String> arguments) {
+  print('Hello world: ${my_homework.calculate()}!');
+}
---
+++ h.w6/my_homework/test/my_homework_test.dart
@@ -0,0 +1,8 @@
+import 'package:my_homework/my_homework.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
