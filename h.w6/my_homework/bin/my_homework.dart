aef8d31e13e93f51ad5151f7f357037bbc11a570
rafa1478 <64263747+rafa1478@users.noreply.github.com>
2025-09-05 17:07:38 +0300

    Delete h.w6 directory

--- h.w6/my_homework/bin/my_homework.dart
+++
@@ -1,5 +0,0 @@
-import 'package:my_homework/my_homework.dart' as my_homework;
-
-void main(List<String> arguments) {
-  print('Hello world: ${my_homework.calculate()}!');
-}
--- h.w6/my_homework/test/my_homework_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:my_homework/my_homework.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
